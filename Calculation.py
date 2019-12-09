de39a6e27660375ae9eaa94bc90e8c3f300d9268
xdd1997 <1837990190@qq.com>
2019-12-09 20:08:42 +0800

    updata

--- Calculation.py
+++ Calculation.py
@@ -9,11 +9,15 @@ print(b)
 import math
 x = 0
 while(1):
-    x +=10
+    x +=1
     if pow(math.e,x)>pow(10,304):
         break
 print('x=',x)
+print(pow(math.e,x))
+#x=700
+#1.0142320547349667e+304
 print(pow(math.e,709))
+#8.218407461554662e+307
 
 
 
